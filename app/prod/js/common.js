fdd0d69158ef3fcea2b6957507b68db68d7b9bfb
Helga13 <oliatrubina@yandex.ru>
2016-01-22 16:56:27 +0300

    delete img.rar

--- app/prod/js/common.js
+++ app/prod/js/common.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     
 $(window).scroll(function () {
 
-    console.log($(document).scrollTop());
+    //console.log($(document).scrollTop());
     if ($(window).scrollTop() >= 0){
         $('header, header.wrapper').animate({'height':'215'},2000);
         $('.bg-opacity').animate({'height':'215', 'background-color':'rgba(0,0,0,1)'},2000);
